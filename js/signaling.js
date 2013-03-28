6527eb9158e7fa30036d2108fc0494b457ae4ecb
Veselin Pizurica <veselin.pizurica@gmail.com>
2013-03-28 11:16:12 +0100

    Added a signaling as the separate js file - signaling.js

---
+++ js/signaling.js
@@ -0,0 +1,92 @@
+var signaling = function(options){
+	
+	var socket = new WebSocket(options.webSocketAddress);	
+	var logg = function(s) { console.log(s); };
+
+	var myId = options.id;
+
+	var callId;
+	var that = {};
+	var offerCallback, answerCallback, presenceCallback, messageCallback;
+
+	that.addPresenceCallback = function(f){
+		presenceCallback = f;
+	};
+
+	that.addMessageCallback = function(f){
+		messageCallback = f;
+	};
+
+	that.addOfferCallback = function(f){
+		offerCallback = f;
+	};
+
+	that.addAnswerCallback = function(f){
+		answerCallback = f;
+	};
+
+	that.sendMessage = function(message, to) {
+        sendMsg(message, to);
+    }
+
+	that.sendPresence = function(_name, stat){
+		_status = stat || 'on';
+		myId = _name;
+        sendMsg({type: 'presence', name: _name, status: _status});
+    }
+
+	that.offer = function(_id){
+		callId = _id;
+		sendMsg({type: 's-offer', origin: myId, id: _id});
+	};
+
+	that.answer = function(_answer){
+		sendMsg({type: 's-answer', origin: myId, answer: _answer});
+	};
+
+    function sendMsg(message, to){
+    	message.from = myId;
+        message.to = callId || to;  
+        var mymsg = JSON.stringify(message);
+        logg("SOCKET Send: " + mymsg);
+        socket.send(mymsg);
+    }
+
+	socket.addEventListener("message", onMessage, false);
+
+    socket.addEventListener("error", function(event) {
+        logg("SOCKET Error: " + event);
+    });
+ 
+    socket.addEventListener("close", function(event) {
+        logg("SOCKET Close: " + event);
+    });
+
+    function onMessage(evt) {
+        logg("RECEIVED: " + evt.data);
+        processSignalingMessage(evt.data);
+    }
+
+    function processSignalingMessage(message) {
+        var msg = JSON.parse(message);
+        logg("processSignalingMessage type(" + msg.type + ")= " + message);
+       
+        if (msg.type === 's-offer' && msg.origin !== myId) {
+        	logg('offer recevied from ', msg.origin);
+        	offerCallback(msg.origin);
+        } else if (msg.type === 's-answer' && msg.origin !== myId) {
+            logg('answer recevied from ' +  msg.origin + '['+ msg.answer + ']');
+            if(msg.answer === 'accept')
+            	answerCallback(msg.answer);
+        } else if (msg.type === 'presence') {
+            logg('presence from: '+ msg.origin);
+            presenceCallback(msg.name, msg.status);
+        } else {
+        	if(messageCallback !== undefined) 
+        		messageCallback(message);
+        }
+    }
+
+    return that;
+
+}
